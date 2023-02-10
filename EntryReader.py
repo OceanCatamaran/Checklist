9db87f3a30b2b0c078dbaa0357f26710d365a8b0
Brendan Grant <1437condoor@gmail.com>
2023-02-10 15:57:23 -0800

    Created three new classes: EntryReader, ObjectListBox2, and gridPlacementTest. These classes are being used to help create the Create C-Sheet Screen.

---
+++ EntryReader.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+class EntryReader:
+
+    def __init__(self, entry):
+        self._entry = entry
+
+    def getNumber(self) -> int:
+        try:
+            outInt = int(self._entry.get())
+            return outInt if outInt >= 0 else 0
+        except Exception:
+            print("Please ensure inputted value is a positive whole number.")
+
+    @classmethod
+    def getCallBack(self, erObj):
+        def entryCallBack():
+            print(erObj.getNumber())
+        return entryCallBack
+
+    
+
+if __name__ == "__main__":
+    root = Tk()
+    eObj = Entry(root)
+    erObj = EntryReader(eObj)
+    getButton = Button(root, command = EntryReader.getCallBack(erObj))
+    eObj.pack()
+    getButton.pack()
+
+    root.mainloop()
+    
