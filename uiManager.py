75bcc1f3f9a589ada2cd2f111273adf6811f6fba
Brendan Grant <1437condoor@gmail.com>
2023-02-17 20:15:44 -0800

    Changed homeUI.py to uiManager.py and renamed the already existing uiManager.py file to homepage.py

---
+++ uiManager.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from frameSignaler import FrameSignaler
+from frameSwitcher import FrameSwitcher
+from testUIA import A
+from testUIB import B
+
+class uiManager:
+    def __init__(self):
+        fsObj = self._getFrameSignaler()
+        top = Tk()
+        FrameSwitcher.addFrame(A.addFrame(top, fsObj))
+        FrameSwitcher.addFrame(B.addFrame(top, fsObj))
+        #checkFrameSignaler checks for any flags, if any then
+        #it switches to appropriate frame.
+        def checkFrameSignaler():
+            print(fsObj.getFlag())
+            print(fsObj.getData())
+            FrameSwitcher.checkFrameSignaler(fsObj)
+            top.after(1000, checkFrameSignaler)
+        checkFrameSignaler()
+        top.mainloop()
+
+    def _getFrameSignaler(self):
+        return FrameSignaler
+
+#    def printFrameSignaler(self, top, fsObj):
+#       print(fsObj.getFlag())
+#       print(fsObj.getData())
+#       top.after(top, 1000, self.printFrameSignaler())
+
+
+
+
+
+
+if __name__ == "__main__":
+    uiManager()
