c46b48f3801ea290d0cdbbca53d72fcffc1f3a84
Ethan Chen <weic1709@gmail.com>
2023-02-23 23:50:18 -0800

    updated homepage

--- homepage.py
+++ homepage.py
@@ -5,17 +5,22 @@ home_page = Tk(className="Home")
 home_page.geometry("500x250")
 
 # create frame for options
-option_frame = Frame(home_page)
+page_frame = Frame(home_page)
 
 # create options buttons for frame
-create_page = Button(option_frame, text="Create", width="50")
-select_page = Button(option_frame, text="Select", width="50")
+create_page = Button(page_frame, text="Create", width="50")
+select_page = Button(page_frame, text="Select", width="50")
 
 create_page.pack(pady=20)
 select_page.pack()
 
-option_frame.place(anchor="s")
-option_frame.pack(padx=5, pady=20)
+page_frame.place(anchor="s")
+page_frame.pack(padx=5, pady=20)
+
+
+# create feedback button
+feedback_button = Button(page_frame, text="?", width="3")
+feedback_button.pack(anchor="w", padx=0, pady=(25, 0))
 
 #Grid.rowconfigure(option_frame, 0, weight=1)
 #Grid.columnconfigure(option_frame, 0, weight=1)
@@ -26,7 +31,7 @@ option_frame.pack(padx=5, pady=20)
 
 create_page.grid
 
-option_frame.place(in_=home_page, anchor="c", relx=0.5, rely=0.5)
+page_frame.place(in_=home_page, anchor="c", relx=0.5, rely=0.5)
 
 # loop window
 home_page.mainloop()
